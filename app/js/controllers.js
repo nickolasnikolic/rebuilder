13418a958a6e1feee803dcb28ea98482f592eb9f
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-07-22 05:37:40 -0500

    styling

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,4 +1,44 @@
-heroesApp.controller('IndexController', ['$scope', '$state', function($scope, $state) {
+heroesApp.controller('IndexController', ['$scope', '$state', 'workplaces', function($scope, $state, workplaces) {
+
+  var substringMatcher = function(strs) {
+    return function findMatches(q, cb) {
+      var matches, substringRegex;
+
+      // an array that will be populated with substring matches
+      matches = [];
+
+      // regex used to determine if a string contains the substring `q`
+      substrRegex = new RegExp(q, 'i');
+
+      // iterate through the pool of strings and for any string that
+      // contains the substring `q`, add it to the `matches` array
+      $.each(strs, function(i, str) {
+        if (substrRegex.test(str)) {
+          matches.push(str);
+        }
+      });
+
+      cb(matches);
+    };
+  };
+
+  $('#jobName').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    source: substringMatcher(workplaces.workplaces)
+  });
+
+  $('#locationName').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    source: substringMatcher(workplaces.cities)
+  });
 
   $('#searchButton').click(function(){
 
@@ -30,20 +70,8 @@ heroesApp.controller('IndexController', ['$scope', '$state', function($scope, $s
 
 }])
 
-heroesApp.controller('HomeController', ['$scope', '$state', function($scope, $state) {
-  document.title = 'heroes - home'; //set the page title
-
-  $('#findJob').click(function(){
-    $state.go('selection');
-  });
-
-}])
-
 heroesApp.controller('SelectionController', ['$scope', '$state', function($scope, $state) {
   document.title = 'heroes - selection'; //set the page title
-
-
-
 
   $scope.page = 1;
   $scope.addMoreJobs = function(){
